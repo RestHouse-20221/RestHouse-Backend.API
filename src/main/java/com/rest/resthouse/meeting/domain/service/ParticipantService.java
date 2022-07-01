f07db6a4384eb09da217d5b4103d3727ff368b68
Mathyu Cardozo <68485205+xMathyu@users.noreply.github.com>
2022-06-30 21:52:21 -0500

    Revert "Merge pull request #2 from RestHouse-20221/feature/meeting"
    This reverts commit f05beb9cf042b668c9d4fa5a5bad96b250ce4658, reversing
    changes made to ebb01916841f7796c0b074e1a5c38aadd1b6ca4c.

--- src/main/java/com/rest/resthouse/meeting/domain/service/ParticipantService.java
+++
@@ -1,9 +0,0 @@
-package com.rest.resthouse.meeting.domain.service;
-
-import com.rest.resthouse.meeting.domain.model.Participant;
-
-import java.util.List;
-public interface ParticipantService {
-    List<Participant> getAllParticipantsByMeetingId(Long meetingId);
-    Participant getParticipantsById(Long participantId);
-}
